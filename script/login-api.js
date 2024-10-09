29baba616534fe7a4d65374f70b3e8d17436c95f
IkramSkei <ikramskei@gmail.com>
2024-10-09 10:32:56 +0700

    login api

--- script/login-api.js
+++ script/login-api.js
@@ -4,15 +4,26 @@ document.getElementById('login-form').addEventListener('submit', function(e) {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    // Username dan password statis untuk simulasi
-    const correctUsername = 'admin';
-    const correctPassword = 'admin123';
+    const loginData = {
+        username: username,
+        password: password
+    };
 
-    if (username === correctUsername && password === correctPassword) {
-        // Login berhasil, arahkan ke dashboard
-        window.location.href = 'index.html';
-    } else {
-        // Tampilkan pesan error jika login gagal
-        alert('Login gagal! Username atau password salah.');
-    }
+    axios.post('https://inventaris-app-backend.vercel.app/api/login', loginData)
+        .then(function(response) {
+            if (response.data.success) {
+               
+                localStorage.setItem('token', response.data.token);
+
+                // Arahkan ke halaman dashboard
+                window.location.href = 'index.html';
+            } else {
+                // Tampilkan pesan error jika login gagal
+                alert('Login gagal! Username atau password salah.');
+            }
+        })
+        .catch(function(error) {
+            console.error('Error saat login:', error);
+            alert('Terjadi kesalahan saat mencoba login.');
+        });
 });
